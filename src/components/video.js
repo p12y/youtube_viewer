968070616d22cc77ad47c0880b5fc4e05d715386
Peter Tyldesley <ptylde@gmail.com>
2017-07-17 15:18:38 +0100

    Add video component

---
+++ src/components/video.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Embed } from 'semantic-ui-react';
+
+
+const Video = (props) => {
+  return (
+    <Embed
+      id='O6Xo21L0ybE'
+      placeholder=''
+      source='youtube'
+    />
+  );
+};
+
+export default Video;
