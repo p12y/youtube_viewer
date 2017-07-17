cf6f77690509288541405868b295f5a5bdc214e8
Peter Tyldesley <ptylde@gmail.com>
2017-07-17 15:00:08 +0100

    Add page layout

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import { Input, Grid, Header } from 'semantic-ui-react';
 
 export default class SearchBar extends Component {
   render() {
     return (
-      <input />
+      <Input size='large' fluid icon='search' placeholder='Search videos...' />
     );
   }
 }
