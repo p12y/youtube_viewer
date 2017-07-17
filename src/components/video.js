40f9acf896aaf1d143616625a93d79c064d1cc0c
Peter Tyldesley <ptylde@gmail.com>
2017-07-17 17:03:39 +0100

    Add video details

--- src/components/video.js
+++ src/components/video.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Embed } from 'semantic-ui-react';
+import { Embed, Card } from 'semantic-ui-react';
 
 
-const Video = (props) => {
+const Video = ({video}) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Embed
-      id='O6Xo21L0ybE'
-      placeholder=''
+    <Card fluid>
+      <Embed
+      id={video.id.videoId}
+      placeholder={video.snippet.thumbnails.medium.url}
       source='youtube'
     />
+    <Card.Content>
+      <Card.Header>{video.snippet.title}</Card.Header>
+      <Card.Meta>
+        <span className='channel'>
+          {video.snippet.channelTitle}
+        </span>
+      </Card.Meta>
+      <Card.Description>
+        {video.snippet.description}
+      </Card.Description>
+    </Card.Content>
+    </Card>
   );
 };
 
