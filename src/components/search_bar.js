5c063c815981ee1cf531d4442b7ea9c201acb235
Peter Tyldesley <ptylde@gmail.com>
2017-07-17 17:39:26 +0100

    Add basic search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,23 @@ import React, {Component} from 'react';
 import { Input, Grid, Header } from 'semantic-ui-react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.props.handleSearch(event.target.value);
+  }
+
   render() {
     return (
-      <Input size='large' fluid icon='search' placeholder='Search videos...' />
+      <Input 
+      size='large' 
+      fluid icon='search' 
+      placeholder='Search videos...' 
+      onChange={this.handleInputChange} />
     );
   }
 }
