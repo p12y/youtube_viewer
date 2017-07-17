bd94cdf3880afb08d9fea77b9097fe2eed3b40ec
Peter Tyldesley <ptylde@gmail.com>
2017-07-17 20:36:24 +0100

    Make input controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,17 +4,19 @@ import {Input} from 'semantic-ui-react';
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {term: ''};
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(event) {
+    this.setState({term: event.target.value});
     this.props.handleSearch(event.target.value);
   }
 
   render() {
     return (
       <Input 
+      value={this.state.term}
       size='large' 
       fluid icon='search' 
       placeholder='Search videos...' 
